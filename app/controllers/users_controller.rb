c7fa092dc8cb28d995ed76f5705399ad74e8fa30
kanaya-pc\kanaya <t.kanaya0530@gmail.com>
2024-12-25 23:32:58 +0900

    userのsession追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to the Sample App!"
+      log_in(@user)
       redirect_to user_url(@user)
     else
       render 'new'
