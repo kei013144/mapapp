38d871cf05b49886493cb41984a8f07ef434744d
kanaya-pc\kanaya <t.kanaya0530@gmail.com>
2024-12-25 22:50:45 +0900

    rails 7.2にダウングレード

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_12_030600) do
-  create_schema "pgagent"
-
+ActiveRecord::Schema[7.2].define(version: 2024_12_25_134046) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "pg_catalog.adminpack"
-  enable_extension "pg_catalog.plpgsql"
-  enable_extension "pgagent.pgagent"
+  enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
     t.string "name"
