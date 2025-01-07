a4986aad5bc56e084d7b482e6f7b42b38796c555
kanaya <t.kanaya0530@gmail.com>
2025-01-07 21:10:00 +0900

    shopreview colon修正

--- app/controllers/shopreview_controller.rb
+++ app/controllers/shopreview_controller.rb
@@ -1,7 +1,7 @@
 class ShopreviewController < ApplicationController
   def create
     shop = Shop.find(params["shop_id"])
-    @shop_review = ShopReview.new(user:current_user, shop:shop, comment: params["comment"])
+    @shop_review = ShopReview.new(user: current_user, shop: shop, comment: params["comment"])
     if @shop_review.save
       render status: :forbidden
     else
