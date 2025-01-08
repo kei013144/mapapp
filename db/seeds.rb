8166c0bd0a26d77d7e5610a91c60de3103a36329
kanaya <t.kanaya0530@gmail.com>
2025-01-08 18:05:24 +0900

    seeds.rb更新

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,6 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Shop.create(name: "八重北食堂", latitude: 35.68249907992816, longitude: 139.76814256519458)
+Shop.create(name: "スターバックス コーヒー ＪＲ東海 東京駅新幹線南ラチ内店", latitude: 35.68055780416288, longitude: 139.7676165793977)
+Shop.create(name: "浅草寿司場ひなと丸 東京駅中央店", latitude: 35.67963439321108, longitude: 139.7678295906337)
