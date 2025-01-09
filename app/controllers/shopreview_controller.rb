f11d008e26b636fde814dae264ba4c09bc6b3ed5
kanaya <t.kanaya0530@gmail.com>
2025-01-09 16:16:04 +0900

    ShopReview修正

--- app/controllers/shopreview_controller.rb
+++ app/controllers/shopreview_controller.rb
@@ -3,7 +3,7 @@ class ShopreviewController < ApplicationController
     shop = Shop.find(params["shop_id"])
     @shop_review = ShopReview.new(user: current_user, shop: shop, comment: params["comment"])
     if @shop_review.save
-      render status: :forbidden
+      redirect_to map_path
     else
       redirect_to root_path
     end
