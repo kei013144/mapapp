a457b58dc3127e4d3753bf7525fcc4416739d969
kanaya <t.kanaya0530@gmail.com>
2025-01-09 13:33:22 +0900

    shop.rb更新

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,3 +1,6 @@
 class Shop < ApplicationRecord
     has_many :shop_reviews, dependent: :destroy
+    validates :name, presence: true
+    validates :latitude, presence: true
+    validates :longitude, presence: true
 end
