2a142e7784e5aef6da87e984dc30a12168402a3e
kanaya <t.kanaya0530@gmail.com>
2025-01-11 02:24:12 +0900

    notification追加

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,5 @@ pin '@hotwired/turbo-rails', to: 'turbo.min.js'
 pin '@hotwired/stimulus', to: 'stimulus.min.js'
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js'
 pin_all_from 'app/javascript/controllers', under: 'controllers'
+pin "@stimulus-components/notification", to: "@stimulus-components--notification.js" # @3.0.0
+pin "stimulus-use" # @0.52.3
