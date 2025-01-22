b1611ba37b8634b38e7d6a618105816edc34e133
kanaya <t.kanaya0530@gmail.com>
2025-01-22 12:38:39 +0900

    flash追加

--- app/controllers/shopreview_controller.rb
+++ app/controllers/shopreview_controller.rb
@@ -3,7 +3,7 @@ class ShopreviewController < ApplicationController
     shop = Shop.find(params['shop_id'])
     @shop_review = ShopReview.new(user: current_user, shop: shop, comment: params['comment'])
     if @shop_review.save
-      redirect_to map_path
+      redirect_to map_path, notice: 'レビューを投稿しました'
     else
       redirect_to root_path
     end
