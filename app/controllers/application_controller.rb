b7ca8770e353494e241c6643e48f02090942eb9d
kanaya <t.kanaya0530@gmail.com>
2025-01-23 21:22:21 +0900

    helper_method修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,22 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
-  include SessionsHelper
+  helper_method :current_user, :logged_in?
+
+  def logged_in?
+    !current_user.nil?
+  end
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
 end
