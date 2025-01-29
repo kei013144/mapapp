0166e396e3e8cdf9711cf06f2b3abdeacc4eb8e4
kanaya <t.kanaya0530@gmail.com>
2025-01-29 18:37:29 +0900

    rubocop更新

--- config/application.rb
+++ config/application.rb
@@ -29,7 +29,6 @@ module Mapapp
 
     # デフォルトのlocaleを日本語(:ja)にする
     config.i18n.default_locale = :ja
-    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
 
     config.generators do |g|
       g.test_framework false
