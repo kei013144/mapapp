f734031f3f76939f1689dfe5e3a5eadb21309897
kanaya <t.kanaya0530@gmail.com>
2025-01-30 21:11:45 +0900

    emailをuniqueにする

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_01_22_175611) do
+ActiveRecord::Schema[7.2].define(version: 2025_01_30_121031) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,6 +40,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_01_22_175611) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "shop_reviews", "shops"
