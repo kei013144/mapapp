46629dd412b1b1ac4369d6ad98669e10a89dce13
kanaya <t.kanaya0530@gmail.com>
2025-02-06 15:19:21 +0900

    users_spec更新

--- spec/models/users_spec.rb
+++ spec/models/users_spec.rb
@@ -17,4 +17,35 @@ RSpec.describe User, type: :model do
     @user.save!
     expect(@user.reload.email).to eq 'sample@sample.jp'
   end
+  it 'nameが必須であること' do
+    @user.name = nil
+    expect(@user).to be_invalid
+    expect(@user.errors[:name]).to include("を入力してください")
+  end
+  it 'emailが必須であること' do
+    @user.email = nil
+    expect(@user).to be_invalid
+    expect(@user.errors[:email]).to include("を入力してください")
+  end
+  it 'emailが正しい形式であること' do
+    @user.email = 'invalid_email'
+    expect(@user).to be_invalid
+    expect(@user.errors[:email]).to include("は不正な値です")
+  end
+  it 'emailが一意であること' do
+    FactoryBot.create(:user, email: 'test@example.com')
+    @user.email = 'test@example.com'
+    expect(@user).to be_invalid
+    expect(@user.errors[:email]).to include("はすでに存在します")
+  end
+  it 'passwordが必須であること' do
+    @user.password = nil
+    expect(@user).to be_invalid
+    expect(@user.errors[:password]).to include("を入力してください")
+  end
+  it 'passwordが6文字以上であること' do
+    @user.password = '12345'
+    expect(@user).to be_invalid
+    expect(@user.errors[:password]).to include("は6文字以上で入力してください")
+  end
 end
