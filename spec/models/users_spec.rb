5f22e0dbef19aacb429b650e95dd7c16fb2ab843
kanaya <t.kanaya0530@gmail.com>
2025-02-06 15:16:31 +0900

    users_spec修正

--- spec/models/users_spec.rb
+++ spec/models/users_spec.rb
@@ -5,20 +5,16 @@ RSpec.describe User, type: :model do
     @user = FactoryBot.build(:user)
   end
 
-  describe 'ユーザー新規登録' do
-    context '新規登録' do
-      it "内容に問題ない場合" do
-        expect(@user).to be_valid
-      end
-      it 'emailが255文字以下のユーザーが作成可能' do
-        @user.email = 'a' * 245 + '@sample.jp'
-        expect(@user).to be_valid
-      end
-      it 'emailは全て小文字で保存される' do
-        @user.email = 'SAMPLE@SAMPLE.JP'
-        @user.save!
-        expect(@user.reload.email).to eq 'sample@sample.jp'
-      end
-    end
+  it "内容に問題ない場合" do
+    expect(@user).to be_valid
+  end
+  it 'emailが255文字以下のユーザーが作成可能' do
+    @user.email = 'a' * 245 + '@sample.jp'
+    expect(@user).to be_valid
+  end
+  it 'emailは全て小文字で保存される' do
+    @user.email = 'SAMPLE@SAMPLE.JP'
+    @user.save!
+    expect(@user.reload.email).to eq 'sample@sample.jp'
   end
 end
