812fa7c3b30f1fc5e0c4830c7c12c196b1562c87
kanaya <t.kanaya0530@gmail.com>
2025-02-06 18:45:06 +0900

    rubocop更新

--- spec/models/users_spec.rb
+++ spec/models/users_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe User, type: :model do
     @user = FactoryBot.build(:user)
   end
 
-  it "内容に問題ない場合" do
+  it '内容に問題ない場合' do
     expect(@user).to be_valid
   end
   it 'emailが255文字以下のユーザーが作成可能' do
@@ -21,32 +21,32 @@ RSpec.describe User, type: :model do
   it 'nameが必須であること' do
     @user.name = nil
     expect(@user).to be_invalid
-    expect(@user.errors[:name]).to include("を入力してください")
+    expect(@user.errors[:name]).to include('を入力してください')
   end
   it 'emailが必須であること' do
     @user.email = nil
     expect(@user).to be_invalid
-    expect(@user.errors[:email]).to include("を入力してください")
+    expect(@user.errors[:email]).to include('を入力してください')
   end
   it 'emailが正しい形式であること' do
     @user.email = 'invalid_email'
     expect(@user).to be_invalid
-    expect(@user.errors[:email]).to include("は不正な値です")
+    expect(@user.errors[:email]).to include('は不正な値です')
   end
   it 'emailが一意であること' do
     FactoryBot.create(:user, email: 'test@example.com')
     @user.email = 'test@example.com'
     expect(@user).to be_invalid
-    expect(@user.errors[:email]).to include("はすでに存在します")
+    expect(@user.errors[:email]).to include('はすでに存在します')
   end
   it 'passwordが必須であること' do
     @user.password = nil
     expect(@user).to be_invalid
-    expect(@user.errors[:password]).to include("を入力してください")
+    expect(@user.errors[:password]).to include('を入力してください')
   end
   it 'passwordが6文字以上であること' do
     @user.password = '12345'
     expect(@user).to be_invalid
-    expect(@user.errors[:password]).to include("は6文字以上で入力してください")
+    expect(@user.errors[:password]).to include('は6文字以上で入力してください')
   end
 end
--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe "Sessions", type: :request do
-  it "ログインページが表示されること" do
+RSpec.describe 'Sessions', type: :request do
+  it 'ログインページが表示されること' do
     get login_path
     expect(response).to have_http_status(200)
-    expect(response.body).to include("ログイン")
+    expect(response.body).to include('ログイン')
   end
 end
