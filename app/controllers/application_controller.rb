d2a8c6b5b84bf924e42cfdc23b59e68d0d8b001f
kanaya <t.kanaya0530@gmail.com>
2025-02-21 16:32:42 +0900

    skip_before_actionの認証回避追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   helper_method :current_user, :logged_in?
+  before_action :require_login
 
   def logged_in?
     !current_user.nil?
@@ -18,5 +19,11 @@ class ApplicationController < ActionController::Base
   def log_out
     session.delete(:user_id)
     @current_user = nil
+  end
+
+  def require_login
+    unless logged_in?
+      redirect_to root_path, notice: "このページにアクセスするにはログインが必要です"
+    end
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+  skip_before_action :require_login, only: [:new, :create]
+  
   def create
     user = User.find_by(email: params[:session][:email].downcase)&.authenticate(params[:session][:password])
     if user
